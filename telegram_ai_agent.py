ff973c8d311a53dfd25028733ec30febbafa2407
pranayx77 <pranaydubru@gmail.com>
2026-03-23 23:44:37 +0530

    Update telegram_ai_agent.py

--- telegram_ai_agent.py
+++ telegram_ai_agent.py
@@ -40,7 +40,7 @@ MODEL              = os.getenv("MODEL", "mistralai/mistral-7b-instruct:free")
 BOT_NAME      = "DexMind"
 BOT_VERSION   = "3.23.26"
 DEVELOPER     = "PraX"
-DEV_TELEGRAM  = "https://t.me/Dex_Error_404"
+DEV_TELEGRAM  = "t.me/Dex_Error_404"
 
 # Timezone — Railway UTC me hota hai, IST set kar diya
 TIMEZONE = ZoneInfo("Asia/Kolkata")
